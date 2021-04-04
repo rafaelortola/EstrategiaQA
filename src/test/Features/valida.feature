6dca6816da461672f1c32150487cf17fd4c60363
Rafael <rafae@LAPTOP-45NL1CCF.(none)>
2021-04-04 18:19:36 -0300

    Ajuste Final

--- src/test/Features/valida.feature
+++
@@ -1,8 +0,0 @@
-#language: pt
-Funcionalidade: segunda feature
-
-  @buscarr
-Cenario: Validar valor do curso igual ao valor da página de detalhes
-Dado que acesso a homepage do estratégia concursos
-Quando clico no link 'por professores'
-Então acesso a tela de cursos por professorr
--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -4,9 +4,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        //plugin = {"java.features.report.CustomFormatter:target/report/Relatorio-Automatizado.html"},
         plugin = {"pretty", "html:C:/Users/rafae/Desktop/DesafioEstrategia/src/test/java/Report"},
-        //plugin = { "de.monochromata.cucumber.report.PrettyReports:target/cucumber" },
         features = {"C:/Users/rafae/Desktop/DesafioEstrategia/src/test/Features"},
         tags = {"@regressivo"},
         glue = {"StepsDefinitions"},
